9618c25b5a92a84673f2a5bc814ee56f2d175a4e
Shraddha M P <114925414+shraddhagowda13@users.noreply.github.com>
2023-07-18 11:28:42 +0530

    Add files via upload

---
+++ division.c
@@ -0,0 +1,11 @@
+//Division of two numbers
+#include<stdio.h>
+int main()
+{
+    int a,b,result=0;
+    printf("Enter two numbers for Divisionn:\n");
+    scanf("%d %d",&a,&b);
+    result =a/b;
+    printf("result=%d",result);
+    return (0);
+}
